033df15174bb41683321f45064646323ef6dd5eb
gearnode <bryan@frimin.fr>
2021-12-03 14:47:25 +0100

    Add dummy syslog interface

---
+++ parser.py
@@ -0,0 +1,9 @@
+class KubernetesParser(object):
+    def init(self, options):
+        return True
+
+    def deinit(self):
+        return True
+
+    def parse(self, message):
+        return True
