b6372a29e80b15641b0b2eb708e19dd9d7cc8c27
gearnode <bryan@frimin.fr>
2021-12-03 15:17:04 +0100

    Parse log message datetime

--- parser.py
+++ parser.py
@@ -1,3 +1,5 @@
+import datetime
+
 class KubernetesParser(object):
     def init(self, options):
         return True
@@ -10,16 +12,25 @@ class KubernetesParser(object):
         if len(message) < 45:
             return False
 
-        level = message[0]
-        month = message[1:3]
-        day = message[3:5]
-        hour = message[6:8]
-        minute = message[9:11]
-        second = message[12:14]
-        sub_second = message[15:21]
-        thread = message[22:29].strip()
-        i = 30 + message[30:].index(":")
-        filename = message[30:i]
-        j = i + 1 + message[i+1:].index("]")
-        line = message[i+1:j]
-        return True
+        try:
+            level = message[0]
+            # The log message format does not contain year.
+            year = datetime.datetime.now().year
+            month = int(message[1:3])
+            day = int(message[3:5])
+            hour = int(message[6:8])
+            minute = int(message[9:11])
+            second = int(message[12:14])
+            microsecond = int(message[15:21])
+            thread = int(message[22:29].strip())
+            i = 30 + message[30:].index(":")
+            filename = message[30:i]
+            j = i + 1 + message[i+1:].index("]")
+            line = message[i+1:j]
+
+            ts = datetime.datetime(year, month, day,
+                                   hour, minute, second, microsecond)
+            
+            return True
+        except ValueError:
+            return False
