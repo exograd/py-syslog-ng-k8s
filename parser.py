42228926b24a5ab3d146611ecbe66f1463ff6a4b
gearnode <bryan@frimin.fr>
2021-12-03 15:05:35 +0100

    Add log header parser logic

--- parser.py
+++ parser.py
@@ -6,4 +6,20 @@ class KubernetesParser(object):
         return True
 
     def parse(self, message):
+        # log message must contains at least 45 characters to be valid.
+        if len(message) < 45:
+            return False
+
+        level = message[0]
+        month = message[1:3]
+        day = message[3:5]
+        hour = message[6:8]
+        minute = message[9:11]
+        second = message[12:14]
+        sub_second = message[15:21]
+        thread = message[22:29].strip()
+        i = 30 + message[30:].index(":")
+        filename = message[30:i]
+        j = i + 1 + message[i+1:].index("]")
+        line = message[i+1:j]
         return True
